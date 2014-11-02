cef8243d33547fc71a3e52bedaf872e8670c11da
xala3pa <salazar3pa@gmail.com>
2014-11-02 13:08:39 +0100

    60 HTTP Handlers

---
+++ 60-HTTP-Handlers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+)
+
+type String string
+
+type Struct struct {
+    Greeting string
+    Punct    string
+    Who      string
+}
+
+func (s String) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+    fmt.Fprint(w, s)
+}
+
+func (h *Struct) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+    fmt.Fprint(w, h)
+}
+
+
+func main() {
+    http.Handle("/string", String("I'm a frayed knot."))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+    // your http.Handle calls here
+    http.ListenAndServe("localhost:4000", nil)
+}
